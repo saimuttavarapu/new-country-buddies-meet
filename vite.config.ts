fe0ed3b8b27bdbd0dbca74ab7b1160ff911ac5d7
Sai MUTTAVARAPU <75469166+saimuttavarapu@users.noreply.github.com>
2025-07-29 11:20:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: '/new-country-buddies-meet/',
+  plugins: [react()],
+});
