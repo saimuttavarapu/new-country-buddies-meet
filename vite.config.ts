8aaa297bf07de1c345129c2c178676a30c08c911
Sai MUTTAVARAPU <75469166+saimuttavarapu@users.noreply.github.com>
2025-07-29 11:48:46 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-
-
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // NOT 'react-swc'
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   base: '/new-country-buddies-meet/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
